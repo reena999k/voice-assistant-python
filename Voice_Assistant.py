4d20fd00c958a38ab789ac8e8c86e9c5b8d416e0
reena999k <reenakushwaha84509@gmail.com>
2026-03-14 00:08:02 +0530

    Add files via upload

---
+++ Voice_Assistant.py
@@ -0,0 +1,11 @@
+# VOICE ASSISTANT
+# Developed by: Reena Kushwaha
+# Internship Project - Oasis Infobyte
+import pyttsx3
+
+engine = pyttsx3.init()
+
+engine.say("Hello Reena, I am your voice assistant")
+engine.say("How can I help you today")
+
+engine.runAndWait()
